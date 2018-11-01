7e2586ab8333b5309cf18879da2da0bbeaca8e44
Baris Ceviz <me@barisceviz.com>
2018-11-02 00:11:34 +0300

    Added some example codes for multiple target frameworks

--- src/MTFExample/FrameworkTests.cs
+++ src/MTFExample/FrameworkTests.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+#if NET472
+    using System.Configuration;
+#elif NETCOREAPP2_0
+    using Microsoft.Extensions.Configuration;
+    using System.IO;
+#elif NETCOREAPP2_1
+    using Microsoft.Extensions.Configuration;
+    using System.IO;
+#endif
 
 namespace MTFExample
 {
@@ -11,11 +20,34 @@ namespace MTFExample
             message = "This is .NET Core App 2.0";
 #elif NETCOREAPP2_1
             message = "This is .NET Core App 2.1";
+#elif NET472
+            message = "This is .NET Framework 4.7.2";
 #elif NETSTANDARD2_0
             message = "This is .NET Standard 2.0";
+#else
+            message = "Cannot detect your framework";
 #endif
 
             return message;
+        }
+
+        public static string GetConfigurationValue(string key)
+        {
+            string value = "";
+
+#if NET472
+            value = ConfigurationManager.AppSettings[key];
+#elif NETCOREAPP2_0 || NETCOREAPP2_1
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+            value = configuration[key];
+#else
+            value = $"Cannot find {key} from any target";      
+#endif
+            
+            return value;
         }
     }
 }
